9e5d58e2117b1daf75d775310e44e0dd78df6240
AlexandrMironovv <asmironovv@gmail.com>
2024-06-08 11:43:55 +0200

    Add workflow wiht PEP8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,14 +1,13 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['asmhomework.hopto.org', '158.160.18.82', 'localhost', '127.0.0.1']# Application definition
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
